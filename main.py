e9f8ada4fa3bc8c668decccf23bee6aef011a2a7
Jjrex8988 <jjrex8988@gmail.com>
2021-05-01 15:07:53 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import requests
+import spotipy
+## https://spotipy.readthedocs.io/en/2.18.0/
+from spotipy.oauth2 import SpotifyOAuth
+
+YOUR_CLIENT_ID ='https://developer.spotify.com/dashboard/login'
+YOUR_CLIENT_SECRET = 'https://developer.spotify.com/dashboard/login'
+
+
+# Scraping Billboard 100
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.find_all("span", class_="chart-element__information__song")
+song_names = [song.getText() for song in song_names_spans]
+
+
+# Spotify Authentication
+## https://spotipy.readthedocs.io/en/2.13.0/#spotipy.oauth2.SpotifyOAuth
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com", ## Use http://example.com as your Redirect URI. You're looking to get the currentuser id (your Spotify username). As per the documentation, make sure you set the redirect URI in the Spotify Dashboard as well.
+        client_id=YOUR_CLIENT_ID,
+        client_secret=YOUR_CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
+
+
+# Searching Spotify for songs by title
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+
+# Creating a new private playlist in Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
+
+
+# Adding songs found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
